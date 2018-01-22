2142e725fdabfaf6fc2841ec0e2c0b30caa938c4
suisuihan <fengbobao@163.com>
2018-01-23 00:12:23 +0800

    升级之路

--- intrestingAlgorigthms/solution.go
+++ intrestingAlgorigthms/solution.go
@@ -34,20 +34,59 @@ func Palindrome(s string)bool{
 	return palindrome(s, 0)
 }
 
+func UpgradeLevel(baseValue int, monsters []int)int{
+	n := len(monsters)
+	if n == 0{
+		return baseValue
+	}
+
+	var gcd func(int, int)int
+	gcd = func(a int, b int)int{
+		if b == 0{
+			return a
+		}else{
+			return gcd(b, a%b)
+		}
+	}
+	ret := baseValue
+	for i:=0; i<n; i++{
+		if ret >= monsters[i]{
+			ret += monsters[i]
+		}else{
+			ret += gcd(ret, monsters[i])
+		}
+	}
+	return ret
+}
 func main(){
+	// tests := []struct{
+	// 	input string
+	// 	expect bool
+	// 	want bool
+	// }{
+	// 	{"coco", true, true},
+	// 	{"coc", true, true},
+	// 	{"cocod", false, true},
+	// 	{"cocodd", false, true},
+	// }
+	// for _, test := range tests{
+	// 	if got := Palindrome(test.input); (got == test.expect) != test.want{
+	// 		fmt.Printf("Palindrome(%s)=%t, except=%t, test failed\n", test.input, got, test.expect)
+	// 	}
+	// }
+
 	tests := []struct{
-		input string
-		expect bool
+		baseValue int
+		monsters []int
+		expect int
 		want bool
 	}{
-		{"coco", true, true},
-		{"coc", true, true},
-		{"cocod", false, true},
-		{"cocodd", false, true},
+		{50, []int{50, 105, 200}, 1101, true},
+		{20, []int{30, 20, 15, 40, 100}, 205, true},
 	}
 	for _, test := range tests{
-		if got := Palindrome(test.input); (got == test.expect) != test.want{
-			fmt.Printf("Palindrome(%s)=%t, except=%t, test failed\n", test.input, got, test.expect)
+		if got := UpgradeLevel(test.baseValue, test.monsters); (got == test.expect) != test.want{
+			fmt.Printf("UpgradeLevel(%d, %v)=%d, except=%d, test failed\n", test.baseValue, test.monsters, got, test.expect)
 		}
 	}
 }
